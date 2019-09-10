bd286f006b1c39b548efa32e5120835a3aa00d3b
Jared Okun <okunj20@gmail.com>
2019-09-10 16:52:49 -0400

    Started State machine code

---
+++ Assets/ControllerInterface.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public interface ControllerInterface
+{
+    void A();
+
+    void B();
+
+    void Up();
+
+    void Down();
+
+    void Left();
+
+    void Right();
+
+}
---
+++ Assets/ControllerStateMachine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State{
+    public float listenPercent;
+    public float randomInputPercent;
+    public float wrongInputPercent;
+    public State(float listenPercent, float randomInputPercent, float wrongInputPercent){
+        this.listenPercent = listenPercent;
+        this.randomInputPercent = randomInputPercent;
+        this.wrongInputPercent = wrongInputPercent;
+    }
+}
+
+
+public class ControllerStateMachine : MonoBehaviour
+{
+
+    public static State DEFAULT = new State(0.8f, 0.1f, 0.05f);
+    public static State HAPPY = new State(0.95f, 0f, 0f);
+    public static State EXCITED = new State(0.7f, 0.5f, 0.1f);
+    public static State ANGRY = new State(0.6f, 0.2f, 0.15f);
+    public static State SAD = new State(0.4f, 0f, 0.15f);
+    public static State DEPRESSED = new State(0.2f, 0f, 0.1f);
+
+    bool timerStarted;
+    float startTime;
+
+    public float randomInputDelay = 5f;
+
+    State currentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = DEFAULT;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
