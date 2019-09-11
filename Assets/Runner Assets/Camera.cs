a4260832defafb91d3f96d72e7a392d60d954ad7
Austin <jacoba4@rpi.edu>
2019-09-11 11:41:48 -0400

    Runner Minigame

---
+++ Assets/Runner Assets/Camera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x+3.5f, 4, -10);
+    }
+
+
+    
+
+}
---
+++ Assets/Runner Assets/Scorebox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorebox : MonoBehaviour
+{
+    public GameObject player;
+    bool pause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pause = player.GetComponent<Player>().pause;
+        if (!pause)
+        {
+            transform.position = new Vector3(player.transform.position.x - 1.23f, 0.5f, 0);
+        }
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Obstacle")
+        {
+            player.GetComponent<Player>().score += 1;
+        }
+    }
+}
