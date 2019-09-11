9e41879519b2908822923e99e515faa2107c71e4
Zaire <Zaire.R.Wilson@gmail.com>
2019-09-11 01:18:27 -0400

    Dialogues System Init

---
+++ Assets/Dialogue Sytem/GameEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    GameEvent Event;
+
+    [SerializeField]
+    UnityEvent[] Response;
+
+    public void OnEnable()
+    {
+        Event.RegisterListener(this);
+    }
+
+    public void OnDisable()
+    {
+        Event.UnregisterListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        for (int i = 0; i < Response.Length; i++)
+            Response[i].Invoke(); 
+    }
+}
