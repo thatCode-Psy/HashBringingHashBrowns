193bdeaa5f024bf7937aa7f0498d73777b73e419
Jared Okun <okunj20@gmail.com>
2019-09-24 17:40:56 -0400

    startScreen was set up

---
+++ Assets/Splashscreens/Scripts/TitleScreenScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenScript : MonoBehaviour
+{
+    float startTime;
+    public float waitTime = 10.5f;
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - startTime >= waitTime){
+            StartGame();
+        }
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene("GameSelect");
+    }
+}
