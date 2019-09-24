a3cb7e15c80c9718e022e5a746d07eaeb1b16128
Jared Okun <okunj20@gmail.com>
2019-09-24 18:41:32 -0400

    fixed dialogue loader glitch in battle minigame. now the scene loads

--- Assets/GameChanAssets/ControllerStateMachine.cs
+++ Assets/GameChanAssets/ControllerStateMachine.cs
@@ -142,7 +142,7 @@ public class ControllerStateMachine : MonoBehaviour
             if(Time.time - pauseStartTime >= pauseInterval && pauseStartTime != -1f){
                 List<int> options = currentGame.GetPossibleDialogueNodes();
                 pauseStartTime = Time.time;
-                if(options.Count > 0){
+                if(options == null || options.Count > 0){
                     Debug.Log("pause");
                     pauseStartTime = -1f;
                     currentGame.Pause();
